c18685bcb8aee11a4b5145eb73a2c787fbd232b1
marmooo <marmooo@users.noreply.github.com>
2021-02-11 13:45:18 +0900

    fix voice-reading

--- docs/index.js
+++ docs/index.js
@@ -7,11 +7,16 @@ loadConfig();
 
 function toggleVoice(obj) {
   if (localStorage.getItem('voice') == 1) {
-    localStorage.setItem('darkMode', 0);
+    localStorage.setItem('voice', 0);
     obj.style.opacity = 0.5;
   } else {
-    localStorage.setItem('darkMode', 1);
+    localStorage.setItem('voice', 1);
     obj.style.opacity = 1.0;
+    speechSynthesis.cancel();
+    let msg = new SpeechSynthesisUtterance('音声読み上げを開始します。');
+    msg.lang = 'ja-JP';
+    msg.voice = japaneseVoices[Math.floor(Math.random() * japaneseVoices.length)];
+    speechSynthesis.speak(msg);
   }
 }
 
@@ -33,7 +38,6 @@ function loadVoices() {
     if (voices.length !== 0) {
       resolve(voices);
     } else {
-
       speechSynthesis.addEventListener("voiceschanged", function() {
         voices = speechSynthesis.getVoices();
         resolve(voices);
